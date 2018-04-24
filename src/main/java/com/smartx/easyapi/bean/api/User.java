660c984a525d29d9742a4649a39e7e3de24e753a
kext <luffyke@gmail.com>
2018-04-24 14:42:45 +0800

    User Builder

--- src/main/java/com/smartx/easyapi/bean/api/User.java
+++ src/main/java/com/smartx/easyapi/bean/api/User.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.UUID;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -19,6 +20,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class User implements Serializable {
 
     private static final long serialVersionUID = -231027225368206064L;
